734f7df82bf0dc7ee2e757ac4e7630463e685359
Maserall Marcelin <maserallm@knights.ucf.edu>
2021-04-06 17:35:08 -0400

    Nell Beamen article

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,87 +25,98 @@ const IndexPage = () => (
       /> */}
       <figure>
         <img
-          src="https://res.cloudinary.com/dtxylaqlc/image/upload/v1613514853/Eastend%20Links%20Photos/E092AB08-6AE3-48E9-8983-2625F85D5DC9_2_eimyxw.jpg"
-          alt="RC FIT 7"
+          src="https://res.cloudinary.com/dtxylaqlc/image/upload/v1617738720/Eastend%20Links%20Photos/Nell_Beamen_-_01_jdhd4m.jpg"
+          alt="Nell Beamen"
         />
 
-        <figcaption>
-          Ryan Clarke <i>CEO</i> of <i>RC FIT 7</i>
-        </figcaption>
+        <figcaption>Nell Beamen</figcaption>
       </figure>
-      <h2>Hard Knocks: RC FIT 7</h2>
+      <h2>STAY BEAMEN</h2>
       <br />
 
       <blockquote>
-        <p>
-          “And do not forget to do good and to share with others, for with such
-          sacrifices God is pleased.”{" "}
-        </p>
-        <figcaption>
-          <i>Hebrews 13:6 NIV </i>
-        </figcaption>
+        <p>I told my cousin, 'Look how much I made off pronouns' </p>
       </blockquote>
 
       <p>
-        Firstly, let's begin by agreeing that anyone who takes the initiative to
-        improve their health by starting their fitness journey is truly
-        admirable. Whether you get active at the gym, ring, or even the great
-        outdoors, taking that leap to achieve your fitness goals is something
-        everyone should strive to do. We here at Eastend Links came upon Ryan
-        Clarke, a personal trainer who originally hailed from Jamaica, who runs
-        his own company called RC FIT 7. The story of Ryan Clarke is a humbling,
-        yet inspiring story brimmed with faith, community, and the love of
-        fitness.
+        Nell Beamen is another local artist who’s been on our radar from
+        Davenport, Florida. Growing up, life was tough in the part of Davenport
+        Beamen lived in. He faced great challenges in his adolescent years that
+        helped shape and develop who he is today. Going from having league
+        dreams playing college football to discovering his talents as a savvy
+        rapper, this is the story of Nell Beamen and how his musical journey
+        continues to rise.
       </p>
 
       <p>
-        Ryan’s journey in the fitness industry began when he was working at one
-        of Publix’s warehouses as a selector. Life for him felt repetitive as he
-        grew tired of falling into the same daily routine with his job and
-        wanted to control more aspects of his life. As he continued to work for
-        the Publix, Ryan was charged with training the associates at his
-        location and his co-workers always encouraged him to pursue a career in
-        personal training. In time, he made the decision to get certified
-        through NASM as a personal trainer and started training part time while
-        still working at Publix.
+        Beamen’s musical journey stems back to his family who were musically
+        inclined. His family had deep ties to the church and served faithfully
+        in that regard. As he spent time with his family in the church and
+        noticed several key family members with the ability to sing, Nell Beamen
+        discovered his own voice. It was easy for him to create music
+        immediately as he is telling his life story with his rhymes. What
+        continuously inspires him to create music is the positive feedback he
+        receives from everyone who supports him, and his own personal enjoyment
+        from creating music.
       </p>
+      <div className="tip tip-right">
+        <a href="https://open.spotify.com/artist/4UuHZsSKP2A5x833MDYye9?si=8GOdtH12RzSJHMlvU7VJug">
+          <img
+            src="https://res.cloudinary.com/dtxylaqlc/image/upload/v1617741027/Eastend%20Links%20Photos/Nell_Beamen_-_The_Cure_Cover_wkheiq.jpg"
+            alt="Nell Beamen The Cure"
+          />
+        </a>
 
+        <figcaption>
+          <i>The Cure</i>
+        </figcaption>
+        <p>
+          Listen to <strong>Nell Beamen's</strong> latest project{" "}
+          <em>
+            <a href="https://www.youtube.com/channel/UC59nWOrCeJu6Lq6Y76zdfDQ/featured">
+              <strong>The Cure </strong>
+            </a>
+          </em>{" "}
+          on{" "}
+          <a href="https://open.spotify.com/artist/4UuHZsSKP2A5x833MDYye9?si=8GOdtH12RzSJHMlvU7VJug">
+            Spotify
+          </a>
+        </p>
+      </div>
       <p>
-        The beginning of RC FIT 7 started with Ryan working out with a group of
-        friends in different parks across Central Florida to help RC FIT 7 gain
-        exposure. Eventually, Ryan came across Celebration, Florida, which is
-        now where RC FIT 7 mainly operates from. Additionally, Ryans wife,
-        Dominique, played a monumental role in the formation of RC FIT 7. She
-        helped Ryan expand the foundational ground-work of the company by doing
-        the majority of the background work, such as setting up their business,
-        marketing, and book-keeping. Realizing that working full-time at Publix
-        was taking so much of his energy away from RC FIT 7, Ryan had to make a
-        life changing decision. After calculating his finances, managing the
-        risk associated with leaving Publix, and receiving some wise counsel,
-        Ryan took a leap of faith and decided to leave Publix to run RC FIT 7
-        full-time.
+        Furthermore, playing football was Nell Beamen’s dream when he was
+        younger. He aspired to play in the NFL and managed to play college
+        football for Laney College and the University of South Alabama. During
+        his college years Beamen began experimenting with rapping during his
+        time on the field. His original rap moniker was just Nell to remain
+        anonymous from his coaches. Beamen made a remix to MadeinTYO’s hit song
+        Uber Everywhere and his team played it during practices and at parties.
+        He didn’t want his coaches to uncover his true identity while he was
+        rapping because they already deemed him to be a failure. The Beamen part
+        of his name originated from the movie Any Given Sunday. Similar to how
+        Willie Beamen vomited on the field, Beamen did the same thing during a
+        scrimmage due to the fact he was hungover during practice. His teammates
+        began to call him Willie Beamen and he decided to embrace it and
+        incorporate it into his rap name today.
       </p>
       {/* <figure> */}
       <img
-        src="https://res.cloudinary.com/dtxylaqlc/image/upload/v1613514853/Eastend%20Links%20Photos/C37B1208-7421-474F-AC68-1F718FB03572_cd5nof.jpg"
-        alt="Big Ass"
+        src="https://res.cloudinary.com/dtxylaqlc/image/upload/v1617738720/Eastend%20Links%20Photos/Nell_Beamen_-_02_lo7hye.jpg"
+        alt="Nell Beamen"
       />
-      <figcaption>Faith Filled</figcaption>
+      <figcaption>I got leg room</figcaption>
       {/* </figure> */}
       <p>
-        Additionally, faith plays a major role in Ryan’s personal life and
-        business. As a follower of Christ, loving other people and loving God is
-        a top priority in his life. As we dive deeper into RC FIT 7 you may
-        notice that their logo is two barbells resembling a cross with a purple
-        sashe, signifying the ultimate sacrifice that Jesus has made for
-        humanity and the sashe highlighting his royal status. Also, the number
-        seven in the company’s name bears significance as well. In scriptures
-        seven is described as the perfect number. Ryan’s goal with this logo and
-        the number seven in his company's name was to highlight his faith and
-        combine that with fitness, to build a community that welcomes all to
-        come as you are. The RC FIT 7 community revolves around the love of
-        fitness, respect for one another, and personal growth. A community truly
-        seeking to build each other up.
+        Additionally, COVID-19 has affected all of our daily lives negatively.
+        As the virus forcibly placed the world on lockdown, the global economy
+        was dramatically spiraling out of control. Most people have lost their
+        jobs and couldn’t find any means to provide for their families. Beamen
+        was also affected by the pandemic as well. During lockdown he couldn’t
+        take trips to the studio which halted him from making music and creating
+        content for his followers. With a stroke of good fortune he prepared
+        himself by having a studio at home, which allowed him to create his
+        latest mixtape called “The Cure.” The project was ultimately named “The
+        Cure,” because of the significant impact COVID has had on our lives.
       </p>
 
       {/* <img
@@ -114,57 +125,49 @@ const IndexPage = () => (
       /> */}
 
       <p>
-        The global pandemic has shifted the dynamics on how we all have to live
-        our daily lives. As the pandemic grew more severe and having a new-born
-        child during these times, Ryan had to shift the way he managed his
-        business, while being safe. Online coaching was Ryan's solution to train
-        his clients during the pandemic, which also allowed him to train people
-        all over the world. Even as things were opening up in Florida, Ryan’s
-        clients were still hesitant on doing face to face training and so was
-        he, due to the fact he was concerned for his clients and his own
-        family’s health. Pre-COVID during group classes, everyone would be
-        high-fiving and hugging each other to motivate the group. Now, nearly a
-        year after the start of the pandemic, most of the RC FIT 7 community
-        follows the CDC’s guidelines to maintain six-feet from each other to
-        reduce the spread of the virus. Furthermore, having a child during the
-        pandemic has forced Ryan to be more structured with his schedule.
-        Before, he was free to work with a client practically anytime, but now
-        his child is his top priority. Although taking care of an infant may be
-        tiring at times, it doesn't stop Ryan from achieving great things with
-        RC FIT 7.
+        Regrettably, Beamen contracted COVID himself. He was aware that he had
+        the virus due to his inability to taste or smell anything (a few
+        symptoms known of the virus). This affected Beamen internally because
+        two weeks prior his newborn daughter was born and he had to remain in
+        self-quarantine until he was cleared from the virus. Unable to spend
+        time with his children, creating content and music to expand his brand,
+        Nell Beamen was stuck in a shell until his time in self-quarantine was
+        over. Now that he is cleared from the virus, He is taking the necessary
+        steps to prevent himself from being reinfected and keep producing high
+        quality music.
       </p>
       <figure>
         <img
-          src="https://res.cloudinary.com/dtxylaqlc/image/upload/v1613514854/Eastend%20Links%20Photos/1DC8279C-3E4D-448E-8042-B183D15E8580_rgcm8x.jpg"
-          alt="Big Ass"
+          src="https://res.cloudinary.com/dtxylaqlc/image/upload/v1617738720/Eastend%20Links%20Photos/Nell_Beamen_-_03_gzcgqd.jpg"
+          alt="Nell Beamen"
         />
 
-        <figcaption>Mind, Body, and Soul</figcaption>
+        <figcaption>Stay linked</figcaption>
       </figure>
 
       <p>
-        Many gyms were closed temporarily due to COVID-19. As most gyms plan on
-        reopening, a lot of concerns began to pile up on our minds. Do they have
-        sanitizer wipes or bottles to clean equipment? How many people are
-        there? Will there be social distancing? Ryan himself thought of these
-        concerns as he is an avid gym rat himself. The majority of RC FIT 7’s
-        training takes place outdoors and Ryan has come up with a creative
-        solution to tackle most of these problems. Ryan’s goal is to create a
-        mobile gym, with the intention of having gym equipment accessible
-        outside for his clients, while continuing outdoor training.
+        Without a doubt, the most difficult aspect of Nell Beamen’s musical
+        journey is being renowned worldwide as a successful artist. The music
+        industry is one of the toughest industries to be successful in as the
+        competition is rampant. The amount of hours an artist must put in the
+        studio, the marketing and the many different dynamics that plays a role
+        in reaching a nationwide audience is a great challenge. Locally, Nell
+        Beamen is a well respected artist with an impressive sound, but from the
+        words of the artist himself, “the world is a bigger place than Polk
+        County.” Regardless of the process of becoming a top rapper, Nell Beamen
+        is taking the necessary steps to making it in the Hip-Hop scene. With
+        his fan-base and local natives of Polk County as a firm foundation, his
+        extensive network of producers, artists, and sound engineers in various
+        cities from Miami to Atlanta is expanding.
       </p>
 
       <p>
-        In the end, Ryan Clarke is a personal trainer who operates RC FIT 7.
-        This story is truly inspirational as it highlights faith, community, and
-        the love of fitness. Those who seek to train at RC FIT 7 can find Ryan
-        in Celebration, Florida and contact him in the links below.
+        In the end, Beamen’s platform is on the rise. As he and his brand
+        continues to grow, we at Eastend Links highly recommend you support Nell
+        Beamen and listen to his most recent release <i>The Cure</i>.
       </p>
 
-      <blockquote>
-        <p>"Lift each other and not let each other down"</p>
-      </blockquote>
-
+      <br />
       <h2>Written By:</h2>
       <blockquote>
         <p>
@@ -178,19 +181,19 @@ const IndexPage = () => (
             fontFamily: "initial"
           }}
         >
-          Links to reach Ryan Clarke:
+          Links to reach Nell Beamen:
           <br />
           <br />
-          <a href="https://www.instagram.com/rcfit7/">
-            <FiInstagram /> Instagram: @rcfit7
+          <a href="https://www.instagram.com/nellbeamen/">
+            <FiInstagram /> Instagram: @nellbeamen
           </a>
           <br />
-          <a href="https://www.facebook.com/RCFit7">
-            <FiFacebook /> Facebook: RC FIT 7
+          <a href="https://twitter.com/NellBeamen">
+            <FiTwitter /> Twitter: @NellBeamen
           </a>
           <br />
-          <a href="mailto:rcfit7@gmail.com">
-            <FiMail /> Email: rcfit7@gmail.com
+          <a href="https://www.facebook.com/NellBeamen">
+            <FiFacebook /> Facebook: NellBeamen
           </a>
         </p>
       </blockquote>
@@ -199,7 +202,7 @@ const IndexPage = () => (
         <p>
           Next Up:
           <br />
-          <Link to="/tbeezi">The Season of the Beez</Link>
+          <Link to="/rcfit">Hard Knocks: RC FIT 7</Link>
         </p>
       </blockquote>
     </article>
---
+++ src/pages/rcfit.js
@@ -0,0 +1,209 @@
+import React from "react";
+import { Link } from "gatsby";
+import { FiInstagram, FiTwitter, FiFacebook, FiMail } from "react-icons/fi";
+// import Img from "gatsby-image"
+
+// import Image from "../components/image"
+import SEO from "../components/seo";
+// import articleCover from "https://res.cloudinary.com/dtxylaqlc/image/upload/v1604155238/woo_eazy_article_header_eeuas5.jpg"
+
+// import "../pages/styles/index.scss"
+
+// export const query = graphql``
+
+// Query MDX to this index page so that articles are brought in by the most recent post.
+// Create Articles Page that contains the previous articles with images of previous interviews
+
+const IndexPage = () => (
+  <>
+    <SEO title="Home" />
+    <article className="post">
+      {/* <Img fixed={} /> */}
+      {/* <img
+        src="../images/woo_eazy_article_header.JPEG"
+        alt="Woo.Marc founder and owner Maserall with FFTE's very own Eazy"
+      /> */}
+      <figure>
+        <img
+          src="https://res.cloudinary.com/dtxylaqlc/image/upload/v1613514853/Eastend%20Links%20Photos/E092AB08-6AE3-48E9-8983-2625F85D5DC9_2_eimyxw.jpg"
+          alt="RC FIT 7"
+        />
+
+        <figcaption>
+          Ryan Clarke <i>CEO</i> of <i>RC FIT 7</i>
+        </figcaption>
+      </figure>
+      <h2>Hard Knocks: RC FIT 7</h2>
+      <br />
+
+      <blockquote>
+        <p>
+          “And do not forget to do good and to share with others, for with such
+          sacrifices God is pleased.”{" "}
+        </p>
+        <figcaption>
+          <i>Hebrews 13:6 NIV </i>
+        </figcaption>
+      </blockquote>
+
+      <p>
+        Firstly, let's begin by agreeing that anyone who takes the initiative to
+        improve their health by starting their fitness journey is truly
+        admirable. Whether you get active at the gym, ring, or even the great
+        outdoors, taking that leap to achieve your fitness goals is something
+        everyone should strive to do. We here at Eastend Links came upon Ryan
+        Clarke, a personal trainer who originally hailed from Jamaica, who runs
+        his own company called RC FIT 7. The story of Ryan Clarke is a humbling,
+        yet inspiring story brimmed with faith, community, and the love of
+        fitness.
+      </p>
+
+      <p>
+        Ryan’s journey in the fitness industry began when he was working at one
+        of Publix’s warehouses as a selector. Life for him felt repetitive as he
+        grew tired of falling into the same daily routine with his job and
+        wanted to control more aspects of his life. As he continued to work for
+        the Publix, Ryan was charged with training the associates at his
+        location and his co-workers always encouraged him to pursue a career in
+        personal training. In time, he made the decision to get certified
+        through NASM as a personal trainer and started training part time while
+        still working at Publix.
+      </p>
+
+      <p>
+        The beginning of RC FIT 7 started with Ryan working out with a group of
+        friends in different parks across Central Florida to help RC FIT 7 gain
+        exposure. Eventually, Ryan came across Celebration, Florida, which is
+        now where RC FIT 7 mainly operates from. Additionally, Ryans wife,
+        Dominique, played a monumental role in the formation of RC FIT 7. She
+        helped Ryan expand the foundational ground-work of the company by doing
+        the majority of the background work, such as setting up their business,
+        marketing, and book-keeping. Realizing that working full-time at Publix
+        was taking so much of his energy away from RC FIT 7, Ryan had to make a
+        life changing decision. After calculating his finances, managing the
+        risk associated with leaving Publix, and receiving some wise counsel,
+        Ryan took a leap of faith and decided to leave Publix to run RC FIT 7
+        full-time.
+      </p>
+      {/* <figure> */}
+      <img
+        src="https://res.cloudinary.com/dtxylaqlc/image/upload/v1613514853/Eastend%20Links%20Photos/C37B1208-7421-474F-AC68-1F718FB03572_cd5nof.jpg"
+        alt="Big Ass"
+      />
+      <figcaption>Faith Filled</figcaption>
+      {/* </figure> */}
+      <p>
+        Additionally, faith plays a major role in Ryan’s personal life and
+        business. As a follower of Christ, loving other people and loving God is
+        a top priority in his life. As we dive deeper into RC FIT 7 you may
+        notice that their logo is two barbells resembling a cross with a purple
+        sashe, signifying the ultimate sacrifice that Jesus has made for
+        humanity and the sashe highlighting his royal status. Also, the number
+        seven in the company’s name bears significance as well. In scriptures
+        seven is described as the perfect number. Ryan’s goal with this logo and
+        the number seven in his company's name was to highlight his faith and
+        combine that with fitness, to build a community that welcomes all to
+        come as you are. The RC FIT 7 community revolves around the love of
+        fitness, respect for one another, and personal growth. A community truly
+        seeking to build each other up.
+      </p>
+
+      {/* <img
+        src="https://res.cloudinary.com/dtxylaqlc/image/upload/v1604155255/2D0E9C3E-0426-4176-A305-EF498BD473C2_rddugu.jpg"
+        alt="Eazy"
+      /> */}
+
+      <p>
+        The global pandemic has shifted the dynamics on how we all have to live
+        our daily lives. As the pandemic grew more severe and having a new-born
+        child during these times, Ryan had to shift the way he managed his
+        business, while being safe. Online coaching was Ryan's solution to train
+        his clients during the pandemic, which also allowed him to train people
+        all over the world. Even as things were opening up in Florida, Ryan’s
+        clients were still hesitant on doing face to face training and so was
+        he, due to the fact he was concerned for his clients and his own
+        family’s health. Pre-COVID during group classes, everyone would be
+        high-fiving and hugging each other to motivate the group. Now, nearly a
+        year after the start of the pandemic, most of the RC FIT 7 community
+        follows the CDC’s guidelines to maintain six-feet from each other to
+        reduce the spread of the virus. Furthermore, having a child during the
+        pandemic has forced Ryan to be more structured with his schedule.
+        Before, he was free to work with a client practically anytime, but now
+        his child is his top priority. Although taking care of an infant may be
+        tiring at times, it doesn't stop Ryan from achieving great things with
+        RC FIT 7.
+      </p>
+      <figure>
+        <img
+          src="https://res.cloudinary.com/dtxylaqlc/image/upload/v1613514854/Eastend%20Links%20Photos/1DC8279C-3E4D-448E-8042-B183D15E8580_rgcm8x.jpg"
+          alt="Big Ass"
+        />
+
+        <figcaption>Mind, Body, and Soul</figcaption>
+      </figure>
+
+      <p>
+        Many gyms were closed temporarily due to COVID-19. As most gyms plan on
+        reopening, a lot of concerns began to pile up on our minds. Do they have
+        sanitizer wipes or bottles to clean equipment? How many people are
+        there? Will there be social distancing? Ryan himself thought of these
+        concerns as he is an avid gym rat himself. The majority of RC FIT 7’s
+        training takes place outdoors and Ryan has come up with a creative
+        solution to tackle most of these problems. Ryan’s goal is to create a
+        mobile gym, with the intention of having gym equipment accessible
+        outside for his clients, while continuing outdoor training.
+      </p>
+
+      <p>
+        In the end, Ryan Clarke is a personal trainer who operates RC FIT 7.
+        This story is truly inspirational as it highlights faith, community, and
+        the love of fitness. Those who seek to train at RC FIT 7 can find Ryan
+        in Celebration, Florida and contact him in the links below.
+      </p>
+
+      <blockquote>
+        <p>"Lift each other and not let each other down"</p>
+      </blockquote>
+
+      <h2>Written By:</h2>
+      <blockquote>
+        <p>
+          {" "}
+          ~<a href="https://www.instagram.com/soirilus_/">Jonathan Soirilus</a>
+        </p>
+        <p
+          style={{
+            textAlign: "center",
+            fontSize: "20px",
+            fontFamily: "initial"
+          }}
+        >
+          Links to reach Ryan Clarke:
+          <br />
+          <br />
+          <a href="https://www.instagram.com/rcfit7/">
+            <FiInstagram /> Instagram: @rcfit7
+          </a>
+          <br />
+          <a href="https://www.facebook.com/RCFit7">
+            <FiFacebook /> Facebook: RC FIT 7
+          </a>
+          <br />
+          <a href="mailto:rcfit7@gmail.com">
+            <FiMail /> Email: rcfit7@gmail.com
+          </a>
+        </p>
+      </blockquote>
+
+      <blockquote>
+        <p>
+          Next Up:
+          <br />
+          <Link to="/tbeezi">The Season of the Beez</Link>
+        </p>
+      </blockquote>
+    </article>
+  </>
+);
+
+export default IndexPage;
